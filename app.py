5e8f6903a60f4ad2e130d27e2b77de2929989b79
ilham aliyudin <ilhampti3@gmail.com>
2020-06-18 19:28:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def insert():
 
 @app.route('/delete/<string:id_data>', methods = ['GET'])
 def delete(id_data):
-    flash("Record Has Been Deleted Successfully")
+    flash("Data telah dihapus")
     cur = mysql.connection.cursor()
     cur.execute("DELETE FROM produk WHERE id=%s", (id_data,))
     mysql.connection.commit()
